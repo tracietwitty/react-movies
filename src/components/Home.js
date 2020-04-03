1bc4654870ae0a8949c6e32a76976370a945a426
Tracie Twite <traciecodes@gmail.com>
2020-04-03 16:44:19 -0500

    Added search bar functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import {
-	API_URL,
-	API_KEY,
 	POSTER_SIZE,
 	IMAGE_BASE_URL,
-	BACKDROP_SIZE
+	BACKDROP_SIZE,
+	SEARCH_BASE_URL,
+	POPULAR_BASE_URL
 } from '../config';
 
 // import components:
@@ -32,11 +32,17 @@ const Home = () => {
 	] = useHomeFetch();
 	const [searchTerm, setSearchTerm] = useState('');
 
+	const searchMovies = search => {
+		const endpoint = search ? SEARCH_BASE_URL + search : POPULAR_BASE_URL;
+
+		setSearchTerm(search);
+		fetchMovies(endpoint);
+	};
+
 	const loadMoreMovies = () => {
-		const searchEndpoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage +
+		const searchEndpoint = `${SEARCH_BASE_URL}${searchTerm}&page=${currentPage +
 			1}`;
-		const popularEndpoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=${currentPage +
-			1}`;
+		const popularEndpoint = `${POPULAR_BASE_URL}&page=${currentPage + 1}`;
 
 		const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
 
@@ -47,12 +53,14 @@ const Home = () => {
 	if (!movies[0]) return <Spinner />;
 	return (
 		<div>
-			<HeroImage
-				image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
-				title={heroImage.original_title}
-				text={heroImage.overview}
-			/>
-			<SearchBar />
+			{!searchTerm && (
+				<HeroImage
+					image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+					title={heroImage.original_title}
+					text={heroImage.overview}
+				/>
+			)}
+			<SearchBar callback={searchMovies} />
 			<Grid header={searchTerm ? 'Search Results' : 'Current Favorites'}>
 				{movies.map(movie => (
 					<MovieThumb
