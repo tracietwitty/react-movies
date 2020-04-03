2271e1f5e6e5f18f2acfec1dd8e9650857b64b24
Tracie Twite <traciecodes@gmail.com>
2020-04-03 14:56:03 -0500

    Brought in hero image from API and styled it

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import {
 	API_KEY,
 	API_BASE_URL,
 	POSTER_SIZE,
+	IMAGE_BASE_URL,
 	BACKDROP_SIZE
 } from '../config';
 
@@ -21,9 +22,15 @@ import { useHomeFetch } from './hooks/useHomeFetch';
 const Home = () => {
 	const [{ state, loading, error }, fetchMovies] = useHomeFetch();
 	console.log(state);
+	if (error) return <div>Something went wrong on our end...</div>;
+	if (!state.movies[0]) return <Spinner />;
 	return (
 		<div>
-			<HeroImage />
+			<HeroImage
+				image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+				title={state.heroImage.original_title}
+				text={state.heroImage.overview}
+			/>
 			<SearchBar />
 			<Grid />
 			<Spinner />
